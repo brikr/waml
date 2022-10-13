866b132ddd8731053b55fc6e1cb2e7db6afdab08
Brik <brikr@users.noreply.github.com>
2022-10-12 18:39:21 -0700

    finish validation tests

--- src/test/validation.test.ts
+++ src/test/validation.test.ts
@@ -1,7 +1,21 @@
+import {WAML, WAMLType} from '../types/waml';
+import {compile} from '../util/compile';
+
 describe('validation', () => {
   test('from and type are mutually exclusive', () => {
     const waml: WAML = {
-      from:
-    }
+      from: 'thing.yml',
+      type: WAMLType.TEXT,
+    };
+
+    expect(() => compile(waml, '')).toThrow('mutually exclusive');
+  });
+
+  test('type must be valid', () => {
+    const waml = {
+      type: 'fake',
+    } as unknown as WAML;
+
+    expect(() => compile(waml, '')).toThrow('unrecognized type');
   });
 });
